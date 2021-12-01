d9396ac6765689fceadcbe8d8579b12620bc280f
Alejo Rodriguez <54213247+alejorodriguez96@users.noreply.github.com>
2021-12-01 21:45:56 +0000

    Builds site w/gh actions [auto]

---
+++ docs/precache-manifest.213d86c03370a2902048b823bb3e3f1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1f4cda46b8359fabf24e0aa41b60978",
+    "url": "/ISFDT24-Map/index.html"
+  },
+  {
+    "revision": "e99e813e8bcdd8745099",
+    "url": "/ISFDT24-Map/static/js/2.96be3947.chunk.js"
+  },
+  {
+    "revision": "b45e14205663e3a520b3e0a455bba542",
+    "url": "/ISFDT24-Map/static/js/2.96be3947.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6022f85b4bc6540785de",
+    "url": "/ISFDT24-Map/static/js/main.f4e51c16.chunk.js"
+  },
+  {
+    "revision": "47f078bd40519c163895",
+    "url": "/ISFDT24-Map/static/js/runtime-main.332c5ff9.js"
+  }
+]);
